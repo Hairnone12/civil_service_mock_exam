6089d5c59c9aebcded16d7b4ffc6c784bf974ca0
Hairnone12 <hernanangkaw12@gmail.com>
2026-09-18 12:14:51 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
 // successful online visit. This file must be a real, separately-hosted
 // file (not inlined) for offline caching to work reliably across reloads.
 
-const CACHE_NAME = "csc-exam-protocol-v4";
+const CACHE_NAME = "csc-exam-protocol-v5";
 
 // The HTML page itself gets cached reactively on first navigation below
 // (so this works no matter what you name the HTML file), but everything
@@ -19,6 +19,7 @@ const APP_SHELL = [
   "./app.js",
   "./icons/icon-192.png",
   "./icons/icon-512.png",
+  "./icons/icon-192-maskable.png",
   "./icons/icon-512-maskable.png",
   "./data/bank.js",
   "./data/final-exam.js",
